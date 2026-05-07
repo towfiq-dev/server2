54156eba2aca31af704d221665cda1cd333ef5d8
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-05-07 15:59:54 +0600

    Auto-update on Thu, May  7, 2026  3:59:54 PM

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://simpleCrudUser:okA0ugjMo8BIRyma@cluster0.d2wubbj.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -28,6 +28,26 @@ const run= async()=> {
       const result = await cursor.toArray()
       res.send(result)
     })
+
+  app.get('/users/:id', async(req, res)=>{
+    const id = req.params.id
+    const query = {
+      _id: new ObjectId(id)
+
+    }
+    const user = await userCollection.findOne(query)
+    res.send(user)
+  })
+
+  app.delete('/users/:id', async(req, res)=>{
+    const id = req.params.id
+    const query = {
+      _id: new ObjectId(id)
+    }
+    const result = await userCollection.deleteOne(query)
+    res.send(result)
+  })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
