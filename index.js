a758170ca0e18c67dc377991b65cb36e6f6535aa
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-05-11 21:51:58 +0600

    Auto-update on Mon, May 11, 2026  9:51:57 PM

--- index.js
+++ index.js
@@ -1,67 +1,46 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.URI
+const uri = process.env.DB_URI
+
 app.use(cors())
 app.use(express.json())
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://simpleCrudUser:okA0ugjMo8BIRyma@cluster0.d2wubbj.mongodb.net/?appName=Cluster0`;
-
 const client = new MongoClient(uri, {
-
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
 });
-const run= async()=> {
-  try {
 
+async function run() {
+  try {
     await client.connect();
-    const db = client.db("simpleCrud");
-    const userCollection = db.collection("users");
+
+    const db = client.db('crud-user')
+    const crudCollection = db.collection('users')
 
     app.get('/users', async(req, res)=>{
-      const cursor = userCollection.find()
-      const result = await cursor.toArray()
+      const result = await crudCollection.find().toArray()
       res.send(result)
     })
-
-  app.get('/users/:id', async(req, res)=>{
-    const id = req.params.id
-    const query = {
-      _id: new ObjectId(id)
-
-    }
-    const user = await userCollection.findOne(query)
-    res.send(user)
-  })
-
-  app.delete('/users/:id', async(req, res)=>{
-    const id = req.params.id
-    const query = {
-      _id: new ObjectId(id)
-    }
-    const result = await userCollection.deleteOne(query)
-    res.send(result)
-  })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-
     //await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res)=>{
-  res.send('Server is running on site')
+  res.send('server is running now')
 })
+
 app.listen(port, ()=>{
-  console.log(`Server is running on port http://localhost:${port}`);
-  
+  console.log(`server is running on port http://localhost:${port}`);
 })
